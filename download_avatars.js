49b3fed19b324adcbf9331631308ccb800df8923
Yichuan wang <44794108+wyc6990906@users.noreply.github.com>
2019-05-06 21:02:37 -0600

    step7

--- download_avatars.js
+++ download_avatars.js
@@ -1,20 +1,35 @@
 var request = require("request");
 var fs = require("fs");
+var secrets = require("./secrets");
 
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...cd === callback;
-  var url =
-    "https://api.github.com/repos/" +
-    repoOwner +
-    "/" +
-    repoName +
-    "/contributors";
+  var options = {
+    url:
+      "https://api.github.com/repos/" +
+      repoOwner +
+      "/" +
+      repoName +
+      "/contributors",
+    headers: {
+      "User-Agent": "request",
+      Authorization: "token " + secrets.GITHUB_TOKEN
+    }
+  };
   request(url, function(err, res, body) {
-    cb(err, body);
+    if (repoOwner === undefined || repoName === undefined) {
+      console.log("You need to pass both parameters");
+    } else {
+      console.log("Welcome to the GitHub Avatar Downloader!");
+    }
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
+  for (let index = 0; index < result.length; index++) {
+    const element = result[index];
+  }
+  downloadImageByURL(element.avatar_url, "avatars/" + index + ".jpg");
   console.log("Errors:", err);
   console.log("Result:", result);
 });
